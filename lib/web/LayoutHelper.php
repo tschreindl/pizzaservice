4b0515c1a52ab32affd559007871213ec3f24b09
tschreindl <tim.schreindl@cn-consult.eu>
2018-01-09 13:50:42 +0100

    Changed LayoutHelper

--- lib/web/LayoutHelper.php
+++ lib/web/LayoutHelper.php
@@ -15,17 +15,26 @@ namespace Pizzaservice\Lib\Web;
  */
 class LayoutHelper
 {
-    function renderHead()
+    function renderHead($_activeMenu)
     {
         $headHTML = "<!DOCTYPE html>\n";
-        $headHTML .= "<html lang=\"en\">\n";
+        $headHTML .= "<html lang=\"de\">\n";
         $headHTML .= "<head>\n";
         $headHTML .= "    <meta charset=\"UTF-8\">\n";
         $headHTML .= "    <title>Pizzaservice</title>\n";
-        $headHTML .= "    <link href=\"" . $this->basePath() . "vendor/twbs/bootstrap/dist/css/bootstrap.css\" rel=\"stylesheet\">\n";
-        $headHTML .= "    <link href=\"" . $this->basePath() . "web/css/main.css\" rel=\"stylesheet\">\n";
         $headHTML .= "    <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js\"></script>\n";
-        $headHTML .= "    <script src=\"" . $this->basePath() . "web/js/javascript.js\"></script>\n";
+        if ($_activeMenu == "pizza")
+        {
+            $headHTML .= "    <link href=\"../vendor/twbs/bootstrap/dist/css/bootstrap.css\" rel=\"stylesheet\">\n";
+            $headHTML .= "    <link href=\"../web/css/main.css\" rel=\"stylesheet\">\n";
+            $headHTML .= "    <script src=\"../web/js/pizza.js\"></script>\n";
+        }
+        else if ($_activeMenu == "order")
+        {
+            $headHTML .= "    <link href=\"../../vendor/twbs/bootstrap/dist/css/bootstrap.css\" rel=\"stylesheet\">\n";
+            $headHTML .= "    <link href=\"../../web/css/main.css\" rel=\"stylesheet\">\n";
+            $headHTML .= "    <script src=\"../../web/js/order.js\"></script>\n";
+        }
         $headHTML .= "</head>\n";
 
         return $headHTML;
@@ -45,43 +54,30 @@ class LayoutHelper
 
         $bodyHTML = "<body>\n";
         $bodyHTML .= "<div class=\"container\">\n";
-        $bodyHTML .= "<div class=\"navbar navbar-default navbar-static-top\">\n";
-        $bodyHTML .= "<a href=\"index.php\" class=\"navbar-brand\">PizzaService</a>\n";
-        $bodyHTML .= "<ul class=\"nav navbar-nav nav-pills\">\n";
+        $bodyHTML .= "    <div class=\"navbar navbar-default navbar-static-top\">\n";
+        $bodyHTML .= "        <a href=\"index.php\" class=\"navbar-brand\">PizzaService</a>\n";
+        $bodyHTML .= "        <ul class=\"nav navbar-nav nav-pills\">\n";
         if ($_activeMenu == "pizza")
         {
-            $bodyHTML .= "<li class=\"nav-item active\"><a href=\"index.php\">Pizzakarte</a></li>\n";
-            $bodyHTML .= "<li class=\"nav-item\"><a href=\"order/index.php\">Bestellung<sup id=\"counter\">" . $counter . "</sup></a></li>\n";
+            $bodyHTML .= "            <li class=\"nav-item active\"><a href=\"index.php\">Pizzakarte</a></li>\n";
+            $bodyHTML .= "            <li class=\"nav-item\"><a href=\"order/index.php\">Bestellung<sup id=\"counter\">" . $counter . "</sup></a></li>\n";
         }
         else if ($_activeMenu == "order")
         {
-            $bodyHTML .= "<li class=\"nav-item\"><a href=\"../index.php\">Pizzakarte</a></li>\n";
-            $bodyHTML .= "<li class=\"nav-item active\"><a href=\"index.php\">Bestellung<sup id=\"counter\">" . $counter . "</sup></a></li>\n";
+            $bodyHTML .= "            <li class=\"nav-item\"><a href=\"../index.php\">Pizzakarte</a></li>\n";
+            $bodyHTML .= "            <li class=\"nav-item active\"><a href=\"index.php\">Bestellung<sup id=\"counter\">" . $counter . "</sup></a></li>\n";
         }
-        $bodyHTML .= "</ul>\n";
-        $bodyHTML .= "</div>\n";
+        $bodyHTML .= "        </ul>\n";
+        $bodyHTML .= "    </div>\n";
 
         return $bodyHTML;
     }
 
     function renderFooter()
     {
-
-
-    }
-
-    private function basePath()
-    {
-        $basedir = realpath(__DIR__ . "../../..");
-        $replace = str_replace($basedir, "", getcwd());
-        $count = substr_count($replace, "\\");
-        $path = "";
-
-        for ($i = 1; $i <= $count; $i++)
-        {
-            $path .= "../";
-        }
-
-        return $path;
+        $footerHTML = "</div>\n";
+        $footerHTML .= "</body>\n";
+        $footerHTML .= "</html>\n";
+        return $footerHTML;
     }
 }
